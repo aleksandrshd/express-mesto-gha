925f1e1b1673a4bab65863a99861afb50d9e3be2
Aleksandr Savichev <aleksandrs.hd@yandex.ru>
2022-11-29 14:32:20 +0300

    подключение к БД изменено с ip на localhost

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/users', usersRouter);
 
 app.use('/cards', cardsRouter);
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 }, () => {
   console.log('Connected to MongoDB');
