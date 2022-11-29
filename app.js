211ffbf63e6b8b53e06c7bceaf304dc6443a0a28
Aleksandr Savichev <aleksandrs.hd@yandex.ru>
2022-11-29 14:07:44 +0300

    подготовлен бэкенд проекта mesto

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6384601d3321b92c4ebc820e',
+  };
+  next();
+});
+
+app.use('/users', usersRouter);
+
+app.use('/cards', cardsRouter);
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+}, () => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
+});
---
+++ controllers/users.js
@@ -0,0 +1,88 @@
+const User = require('../models/user');
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    return res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: 'Произошла ошибка' });
+  }
+};
+
+const createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    return res.status(201).json(user);
+  } catch (err) {
+    console.error(err);
+    if (err.name === 'ValidationError') {
+      const errors = Object.values(err.errors).map((error) => error.message);
+      return res.status(400).json({ message: `Переданы некорректные данные при создании пользователя. ${errors.join(', ')}` });
+    }
+    return res.status(500).json({ message: 'Произошла ошибка' });
+  }
+};
+
+const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'Пользователь не найден' });
+    }
+
+    return res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: 'Произошла ошибка' });
+  }
+};
+
+const updateUserProfile = async (req, res) => {
+  try {
+    const id = req.user._id;
+    const user = await User.findByIdAndUpdate(id, { name: req.body.name, about: req.body.about }, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: 'Пользователь не найден' });
+    }
+
+    return res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    if (err.name === 'ValidationError') {
+      const errors = Object.values(err.errors).map((error) => error.message);
+      return res.status(400).json({ message: `Переданы некорректные данные при обновлении профиля. ${errors.join(', ')}` });
+    }
+    return res.status(500).json({ message: 'Произошла ошибка' });
+  }
+};
+
+const updateUserAvatar = async (req, res) => {
+  try {
+    const id = req.user._id;
+    const user = await User.findByIdAndUpdate(id, { avatar: req.body.avatar }, { new: true });
+
+    if (!user) {
+      return res.status(404).json({ message: 'Пользователь не найден' });
+    }
+
+    return res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    if (err.name === 'ValidationError') {
+      const errors = Object.values(err.errors).map((error) => error.message);
+      return res.status(400).json({ message: `Переданы некорректные данные при обновлении аватара. ${errors.join(', ')}` });
+    }
+    return res.status(500).json({ message: 'Произошла ошибка' });
+  }
+};
+
+module.exports = {
+  getUsers, createUser, getUser, updateUserProfile, updateUserAvatar,
+};
