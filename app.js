11a6a1e6cc9dcc169475fb679fa4fc3a7f834b9c
Aleksandr Savichev <aleksandrs.hd@yandex.ru>
2022-12-15 15:32:45 +0300

    s_a_pc_7

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ app.post('/signin', celebrate({
 
 app.post('/signup',celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().regex(urlRegEx).required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(urlRegEx),
     email: Joi.string().email().required(),
     password: Joi.string().required().min(4),
   }),
